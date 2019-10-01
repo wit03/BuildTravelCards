c8526dad96aad84b2445ac8ae2217530ac4a00a4
lyrafelipe <appslyraf@gmail.com>
2019-10-01 10:58:14 -0300

    adds punta cana trip

--- src/response/response.js
+++ src/response/response.js
@@ -1,6 +1,6 @@
  
  const response = [
-    {
+  {
     "name": "Weekend in Vegas",
     "image": 'vegas.jpg',
     "dateFrom": "May 7th 2020",
@@ -8,8 +8,8 @@
     "guests": 2,
     "category": "weekend break",
     "copy": "Next level vexillologist taiyaki migas cloud bread post-ironic. Offal selvage gastropub bitters."
-},
-{
+  },
+  {
     "name": "Hiking in the Peaks",
     "image": 'peaks.jpg',
     "dateFrom": "July 6th 2020",
@@ -27,6 +27,15 @@
     "category":"family",
     "copy": "Air plant tattooed schlitz pinterest heirloom. Knausgaard gastropub chartreuse."
   },
+  {
+    "name": "Relaxing in Punta Cana",
+    "image": 'puntacana.jpg',
+    "dateFrom": "Aug 7th 2020",
+    "dateTo": "Aug 9th 2020",
+    "guests": 2,
+    "category":"weekend break",
+    "copy": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum a maximus dolor. Sed iaculis luctus metus a mattis."
+  }
 ]
 
 export default response;
